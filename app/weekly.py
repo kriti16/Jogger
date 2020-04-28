c114f6841b32fe62bc56ae3a93530dfdd989797a
Kriti Joshi <kritij.1601@gmail.com>
2020-04-28 23:23:01 +0530

    Improve email format

--- app/weekly.py
+++ app/weekly.py
@@ -1,11 +1,13 @@
 from datetime import date, timedelta
 from flask_mail import Message
+from flask import render_template
 
 from app.database.models import User, Record
 
-def send_email(mail, subject, sender, recipient, text_body):
+def send_email(mail, subject, sender, recipient, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=[recipient])
     msg.body = text_body
+    msg.html = html_body
     mail.send(msg)
 
 def generate_weekly_report(app, mail):
@@ -25,6 +27,9 @@ def generate_weekly_report(app, mail):
 			print('avg_distance ', avg_distance)
 			print('avg_speed ', avg_speed)
 
-			text_body = 'avg_distance '+ str(avg_distance) + ' avg_speed ' + str(avg_speed)
-
-			send_email(mail, 'Weekly jogger report', app.config['MAIL_USERNAME'], s.email, text_body)
+			text_body = 'Weekly report'
+			# text_body = 'avg_distance '+ str(avg_distance) + ' avg_speed ' + str(avg_speed)
+			html_body = render_template('mail.html', username=s.username, 
+				avg_speed=round(avg_speed, 2), avg_distance=round(avg_distance,2))
+			send_email(mail, 'Weekly jogger report', app.config['MAIL_USERNAME'], s.email, text_body,
+				html_body)
